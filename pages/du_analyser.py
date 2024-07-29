739d6207a89c874949c412fd3efd70f3e1d5e8e4
Aunn <anjulbhatia@gmail.com>
2024-07-29 09:15:45 +0530

    First App

---
+++ pages/du_analyser.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("DU Analyser")
+st.write("Analyze various aspects of Delhi University here.")
+# Add more content and functionality as needed
---
+++ scripts/apptexts.py
@@ -0,0 +1,61 @@
+bio = '''
+**Inside DU** is a community that brings to you what you need the most right now.
+
+🔸Instant doubt solving
+
+🔸Live sessions
+
+🔸Admission updates
+
+🔸College & Course review
+
+🔸Preference list review
+
+🔸Personalised preference Sheet
+
+🔸Career Guidance 
+
+🔸And DU ke andar ki baatein
+
+A community build by students who are from North Campus by merit and happens to be cousins by coincidence that understands your problems because we have faced them first hand.
+
+Mentorship begins after the community reaches 200 members
+
+Share with your friends who may get benefitted from this.
+[bit.ly/InsideDU_WACommunity](https://bit.ly/InsideDU_WACommunity)
+
+Also support us on Instagram: [instagram.com/edu.insidedu](https://instagram.com/edu.insidedu)
+
+**2023-2024 Statistics**
+**200+** member community  
+**10+** seniors mentoring y'all     
+**150+** college converts through our personalized preference sheets    
+**2** Live sessions    
+Instant doubt resolution     
+Career Guidance    
+
+Our Success Stories:  
+
+🔹**SRCC** - Economics Hons
+
+🔹**LSR** - English Hons, BCom Hons, Mathematics Hons
+
+🔹**Hindu** - Bcom Hons, Mathematics Hons, Statistics Hons
+
+🔹**Hansraj** - Bcom Hons, BA Prog
+
+🔹**KMC** - Economics Hons, Statistics Hons
+
+🔹**Ramjas** - BCom Hons, Physics hons 
+
+🔹**Miranda House** - Mathematics Hons, BA Prog
+
+🔹**DRC** - BA Prog, Zoology Hons
+
+🔹**IPCW** - BCom Hons, BA Prog
+
+🔹KMV - CS hons
+
+🔹**SGGSCC, SGTB Khalsa, Gargi, JMC, ARSD and more...**
+
+'''
