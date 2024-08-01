be8744a7c5237512fc307f4fe979be5bef0daa96
Aunn <anjulbhatia@gmail.com>
2024-08-01 18:25:44 +0530

    First App 3.0

--- pages/1_DU_Analyser.py
+++ pages/1_DU_Analyser.py
@@ -2,13 +2,6 @@ import streamlit as st
 import pandas as pd
 import datasets.cuet_data as cuet_data
 
-st.set_page_config(
-    page_title="DU Analyser | InsideDU",
-    page_icon="🌟",
-    layout="centered",
-    initial_sidebar_state="collapsed",
-)
-
 # Add custom CSS to hide the GitHub icon
 hide_github_icon = """
 <style>
